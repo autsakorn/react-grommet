b539bda765894410f73f4c403191abd182598efc
autsakorn <autsakorn@Autsakorns-MacBook-Pro.local>
2017-03-24 10:09:08 +0700

    s

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+// import Grommet from './index-commonjs';
+import {Box, Section, Title} from 'grommet/components';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +15,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Title>Hello World</Title>
       </div>
     );
   }
